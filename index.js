c45d05b95b708b63c8b60ce5a6ace750462b0dea
lucabecci <beccibrian@gmail.com>
2021-07-28 20:37:36 -0300

    chore(): newsupdates, libraries and scripts of project

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-
-
-class Node {
-    constructor(value, node = null){
-        this.value = value
-        this.node = node
-    }
-}
-
-const node1 = new Node(22)
-const node2 = new Node(33, node1)
-
-export default Node
+(() => console.log("Welcome to my repo"))()
