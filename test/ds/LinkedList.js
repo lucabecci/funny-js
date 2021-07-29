f82e7cb884c4f464348e4759f8f8c6e2590e27c5
lucabecci <beccibrian@gmail.com>
2021-07-29 00:12:41 -0300

    tets(): all tests finished for Linked List

--- test/ds/LinkedList.js
+++ test/ds/LinkedList.js
@@ -84,13 +84,99 @@ describe('LinkedList', function () {
         
         done()
     })
-});
 
-/*
-const node6 = new Node(609);
-const node5 = new Node(12, node6);
-const node4 = new Node(321, node5);
-const node3 = new Node(1, node4);
-const node2 = new Node(9789, node3);
-const node1 = new Node(13, node2);
-*/
+    it("delete tail - LinkedList", function(done){
+        this.timeout(9999)
+        const linkedList = new LinkedList();
+        linkedList.append(23)
+        linkedList.append(241)
+        linkedList.append(1)
+
+        linkedList.deleteTail()
+        assert.deepEqual(linkedList.tail.value, 241)
+        done()
+    })
+
+    it("delete head - LinkedList", function(done){
+        this.timeout(9999)
+        const linkedList = new LinkedList();
+        linkedList.append(23)
+        linkedList.append(241)
+        linkedList.append(1)
+        linkedList.deleteHead()
+        assert.deepEqual(linkedList.head.value, 241)
+        done()
+    })
+
+    it("find existent value in list - LinkedList", function(done){
+        this.timeout(9999)
+        const linkedList = new LinkedList();
+        linkedList.append(23008924)
+        linkedList.append(241657)
+        linkedList.append(154)
+        linkedList.append(2323)
+        linkedList.append(24123)
+        linkedList.append(189374)
+        linkedList.append(2389273.32)
+        linkedList.append(24198127)
+        linkedList.append(-26978)
+
+        const item = linkedList.find({value: 2389273.32});
+        assert.deepEqual(item.value, 2389273.32)
+        done()
+    })
+
+    it("find node with callback - LinkedList", function(done){
+        this.timeout(9999)
+        const linkedList = new LinkedList()
+        linkedList.append({value: 241657, key: "one"})
+        linkedList.append({value: 154, key: "two"})
+        linkedList.append({value: 2323, key: "three"})
+
+        const item = linkedList.find({callback: (v) => v.key === "two"})
+        assert.deepEqual(item.value.value, 154)
+        done()
+    })
+
+    it("create linked list with an array - LinkedList", function(done){
+        this.timeout(9999)
+        const linkedList = new LinkedList()
+        linkedList.fromArray([23,343,656,232,757,12,542312,923.201])
+        const item = linkedList.find({value: 757})
+        assert.deepEqual(item.value, 757)
+        done()
+    })
+
+    it("transform linked list to array - LinkedList", function(done){
+        this.timeout(9999)
+        const linkedList = new LinkedList()
+        linkedList.append(23008924)
+        linkedList.append(241657)
+        linkedList.append(154)
+        linkedList.append(2323)
+        linkedList.append(24123)
+        linkedList.append(189374)
+        linkedList.append(2389273.32)
+        linkedList.append(24198127)
+        linkedList.append(-26978)
+
+        const array = linkedList.toArray()
+        assert.deepEqual(array.length, 9)
+        assert.deepEqual(array[array.length - 1].value, -26978)
+        done()
+    })
+
+    it("reverse linked list - LinkedList", function(done){
+        this.timeout(9999)
+        const linkedList = new LinkedList()
+        linkedList.fromArray([23,343,656,232,757,12,542312,923.201])
+
+        assert.deepEqual(linkedList.head.value, 23)
+        assert.deepEqual(linkedList.tail.value, 923.201)
+        //revert
+        linkedList.reverse()
+        assert.deepEqual(linkedList.tail.value, 23)
+        assert.deepEqual(linkedList.head.value, 923.201)
+        done()
+    })
+});
